464403f01909f4a008ca189eb3d58e478bb97357
Brian Clifton <brian@scalemodel.com>
2016-02-06 17:59:35 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,101 @@
+var MAP_WIDTH = 369;
+var MAP_HEIGHT = 170;
+var pixelColorMap = {};
+
+
+
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
+// Dropzone.options.myAwesomeDropzone = {
+//   init: function() {
+//     this.on("addedfile", function(file) {
+//       alert("Added file.");
+//      });
+//   }
+// };
+
+
+// function grayscale(image, bPlaceImage) {
+//   var myCanvas=document.createElement("canvas");
+//   var myCanvasContext=myCanvas.getContext("2d");
+//
+//   var imgWidth=image.width;
+//   var imgHeight=image.height;
+//   // You'll get some string error if you fail to specify the dimensions
+//   myCanvas.width= imgWidth;
+//   myCanvas.height=imgHeight;
+//   //  alert(imgWidth);
+//   myCanvasContext.drawImage(image,0,0);
+//
+//   // This function cannot be called if the image is not rom the same domain.
+//   // You'll get security error if you do.
+//   var imageData=myCanvasContext.getImageData(0,0, imgWidth, imgHeight);
+//
+//   // This loop gets every pixels on the image and
+//     for (j=0; j<imageData.height; i++)
+//     {
+//       for (i=0; i<imageData.width; j++)
+//       {
+//          var index=(i*4)*imageData.width+(j*4);
+//          var red=imageData.data[index];
+//          var green=imageData.data[index+1];
+//          var blue=imageData.data[index+2];
+//          var alpha=imageData.data[index+3];
+//          var average=(red+green+blue)/3;
+//    	     imageData.data[index]=average;
+//          imageData.data[index+1]=average;
+//          imageData.data[index+2]=average;
+//          imageData.data[index+3]=alpha;
+//        }
+//      }
+//
+//     if (bPlaceImage)
+// 	{
+// 	  var myDiv=document.createElement("div");
+// 	     myDiv.appendChild(myCanvas);
+// 	  image.parentNode.appendChild(myCanvas);
+// 	}
+// 	return myCanvas.toDataURL();
+// }
+
+// function getBase64Image(img) {
+//     // Create an empty canvas element
+//     var canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+//
+//     // Copy the image contents to the canvas
+//     var ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0);
+//
+//
+//     console.log(ctx);
+//     // Get the data-URL formatted image
+//     // Firefox supports PNG and JPEG. You could check img.src to
+//     // guess the original format, but be aware the using "image/jpg"
+//     // will re-encode the image.
+//     var dataURL = canvas.toDataURL("image/png");
+//
+//     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// }
+
+
+
+
+
+
 $(document).ready(function(){
+
+  $(":file").change(function () {
+      if (this.files && this.files[0]) {
+          var reader = new FileReader();
+          reader.onload = imageIsLoaded;
+          reader.readAsDataURL(this.files[0]);
+      }
+  });
 
   var colorMap = ['rgba(0,0,0,0)',
                   'rgba(206,226,249, 0.25)',
@@ -22,7 +114,7 @@ $(document).ready(function(){
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v8',
         center: [-98.5795, 39.828175],
-        zoom: 4
+        zoom: 3
     });
 
     console.log(grid_data);
@@ -98,7 +190,7 @@ $(document).ready(function(){
       var allPaths = d3.selectAll('path')
         .each(function(d){
           d3.select(this)
-            .attr('i', getRandomIntInclusive(0, 6) )
+            .attr('i', getRandomIntInclusive(0,6))
             .attr('fill', function(d){
               var current = d3.select(this);
               var c = parseInt(current.attr('i'));
@@ -107,25 +199,88 @@ $(document).ready(function(){
       });
     });
 
-    var rowCount = 0;
+    // $('#count').on('click', function(e){
+    //   d3.selectAll('path')
+    //     .each(function(){
+    //       d3.select(this)
+    //         .attr('fill', function(d){
+    //           var current = d3.select(this);
+    //           var fid = parseInt(current.attr('fid'));
+    //             if (fid % 85 == 0){
+    //               return  colorMap[5];
+    //             } else {
+    //               return colorMap[1];
+    //             }
+    //           });
+    //     });
+    //   });
 
-    $('#count').on('click', function(e){
+
+
+    $('#noise').on('click', function(e){
+
+      var valueMap = {};
+
+      for (var x = 0; x < MAP_WIDTH; x++) {
+        for (var y = 0; y < MAP_HEIGHT; y++) {
+
+          var value = Math.abs(noise.perlin2(x / 100, y / 100));
+          value *= 256;
+          // var cell = (x + y * MAP_WIDTH) * 4;
+
+          valueMap[ x + ',' + y ] = value
+          // perlinNoiseMap[]
+
+
+          // data[cell] = data[cell + 1] = data[cell + 2] = value;
+          // data[cell] += Math.max(0, (25 - value) * 8);
+          // data[cell + 3] = 255; // alpha.
+
+
+        }
+      }
+
+
+      console.log(valueMap);
+
+
       d3.selectAll('path')
         .each(function(){
           d3.select(this)
             .attr('fill', function(d){
               var current = d3.select(this);
               var fid = parseInt(current.attr('fid'));
-              // if (fid % 680 == 0){
-              if (fid % 85 == 0){
-                return  colorMap[5];
-              } else {
-                return colorMap[1];
-              }
+
+              return fid;
+              // var x = current.attr('x');
+              // var y = current.attr('y');
 
             });
         });
-
     });
-  });
-});
+
+
+
+
+    function imageIsLoaded(e) {
+      getBase64Image(e.target.result);
+
+      d3.selectAll('path')
+        .each(function(){
+          d3.select(this)
+            .attr('fill', function(d){
+              var current = d3.select(this);
+              var fid = parseInt(current.attr('fid'));
+
+              return fid;
+              // var x = current.attr('x');
+              // var y = current.attr('y');
+
+            });
+        });
+    };
+
+
+  }); // ends d3.json()
+
+}); // ends $(document).ready()
